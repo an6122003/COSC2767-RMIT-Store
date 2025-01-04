db2c59c6ed321ab25885bf05364f65e928759e73
an6122003 <87111915+an6122003@users.noreply.github.com>
2025-01-04 21:18:20 +0000

    16

--- client/app/components/Common/Footer/index.js
+++ client/app/components/Common/Footer/index.js
@@ -65,7 +65,7 @@ const Footer = () => {
           </div>
         </div>
         <div className='footer-copyright'>
-          <span>© {new Date().getFullYear()} RMIT Store. All rights reserved. Edit 15</span>
+          <span>© {new Date().getFullYear()} RMIT Store. All rights reserved. Edit 16</span>
         </div>
         <ul className='footer-social-item'>
           <li>
