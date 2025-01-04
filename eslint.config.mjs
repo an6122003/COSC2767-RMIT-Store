b7ab18fcec36588a13a10e20fc71dbe2a65504af
an6122003 <87111915+an6122003@users.noreply.github.com>
2025-01-04 19:32:04 +0000

    Add eslint config for nodejs

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginReact.configs.flat.recommended,
+  {rules: {
+    "react/prop-types": "off", // Disable react/prop-types globally
+  }}
+];
